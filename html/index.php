b1a5878559215cddbb981dc92315da97b2b5bd9c
OriginalNexus <razvan.neacsu5@gmail.com>
2018-03-24 11:17:44 +0200

    Add index.php

---
+++ html/index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>FoodRAR</title>
+	</head>
+	<body>
+		<h1>Șatra BENZ - Șatra se întoarce din nou</h1>
+		<pre>
+Dăm foc la TOT,
+Că noi suntem B. E. N. Z. INA! - INA x3
+Din căruţă în trending, în trening de firmă,
+FIRMĂ x3 avem FIRMĂ!
+Facem şi la vedere banii, da' şi ca lăutarii,
+Luăm COCArii, luăm COCArii, noi 5 Pablo Escobarii.
+Ridicăm sus TOT,
+Noi suntem ADRENALINA - INA x3
+Din căruţă în trending, în trening de firmă,
+FIRMĂ x3 avem FIRMĂ!
+Facem şi la vedere banii, da' şi ca lăutarii,
+Luăm COCArii, luăm COCArii, noi 5 Pablo Escobarii.
+		</pre>
+	</body>
+</html>
