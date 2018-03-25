bbc509249c29620b69478e050409789949a3a2fa
OriginalNexus <razvan.neacsu5@gmail.com>
2018-03-25 07:34:24 +0300

    Add session for collector

--- html/collector/index.php
+++ html/collector/index.php
@@ -1,3 +1,5 @@
+<?php require_once('setup.php'); ?>
+
 <!DOCTYPE HTML>
 
 <html>
@@ -31,34 +33,13 @@
         </a>
 
         <div class="brand-logo right collectorName">
-          Gigel Gigica
+          <?php print($_SESSION['user']['name']); ?>
         </div>
 
       </div>
     </nav>
 
-    <?php
 
-    require_once('../setup.php');
-
-    if (!isset($_SESSION['collector'])) {
-    	http_response_code(401);
-    	die('Not logged in');
-    }
-
-    $stmt = $db_conn->prepare('select posts.*, status.display_name as status_display_name from posts inner join status on status.name=posts.status where posts.user_email = ?');
-    $stmt->bind_param("s", $_SESSION['collector']['email']);
-
-    if (!$stmt->execute()) {
-    	http_response_code(500);
-    	die('Could not fetch posts');
-    }
-
-    $result = $stmt->get_result();
-
-    print(json_encode($result->fetch_all(MYSQLI_ASSOC)));
-
-    ?>
 
     <script
       src="https://code.jquery.com/jquery-3.3.1.min.js"
---
+++ html/collector/setup.php
@@ -0,0 +1,25 @@
+<?php
+
+	session_start();
+
+	if (!isset($_SERVER['PHP_AUTH_USER'])) {
+	    die('Server error');
+	}
+
+	$db_conn = new mysqli('10.81.146.145', 'foodrar', 'alinarazvanx2', 'foodrar');
+	if ($db_conn->connect_error) {
+		die('Could not connect to database: ' . $db_conn->connect_error);
+	}
+	$db_conn->set_charset("utf8");
+
+	$stmt = $db_conn->prepare('select * from collector where email = ?');
+	$stmt->bind_param("s", $_SERVER['PHP_AUTH_USER']);
+
+	if (!$stmt->execute()) {
+		http_response_code(500);
+		die('Server error');
+	}
+
+	$_SESSION['user'] = $stmt->get_result()->fetch_assoc();
+
+?>
